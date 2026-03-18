17a63bbc435d838d2235b0da4b651bdb2a64c160
Leela <leelanarayan1214@gmail.com>
2026-03-18 14:29:32 +0530

    HTML files are Fixed with template inheritance

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
+@app.route('/welcome', methods=['POST'])
+def welcome():
+    username = request.form['username']
+    return render_template('dashboard.html', username=username)
+
 
 
 if __name__ == '__main__':
